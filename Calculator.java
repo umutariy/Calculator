ba210ba9542740894b8f5d34f6bf7fae8dc4b8b3
Umutari <umutariy@gmail.com>
2017-01-20 17:20:41 +0000

    my first commit

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator{
+  public static void main(String[] parameters){
+    if(parameters[0].equals("add")){
+      double num1=Double.parseDouble(parameters[1]);
+      double num2=Double.parseDouble(parameters[2]);
+      System.out.println(num1+num2);
+    }
+  }
+}
+

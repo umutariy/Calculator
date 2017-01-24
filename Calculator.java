d2639ac6b1a9197153b5d7f76565cab407e2e3f8
Umutari <umutariy@gmail.com>
2017-01-24 21:04:44 +0000

    changes made

--- Calculator.java
+++ Calculator.java
@@ -5,6 +5,11 @@ public class Calculator{
       double num2=Double.parseDouble(parameters[2]);
       System.out.println(num1+num2);
     }
+    else if(parameters[0].equals("subtract")){
+      double num1=Double.parseDouble(parameters[1]);
+      double num2=Double.parseDouble(parameters[2]);
+      System.out.println(num1-num2);
+    }
   }
 }
 
